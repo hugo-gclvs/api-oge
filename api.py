5ed515fbb9c94d056c2ea73043bb1e8f60b66152
Hugoreoo <hugo.gclvs@icloud.com>
2023-12-07 18:34:50 +0100

    refact code

--- api.py
+++ api.py
@@ -1,65 +1,57 @@
 import re
-from getpass import getpass
 import requests
 from bs4 import BeautifulSoup
-
+from getpass import getpass
 
 class API:
     def __init__(self, user=input("Username: "), pwd=getpass("Password: ")):
         self.username = user
-        self.pwd = pwd
-        self.URL_ACCUEIL = "https://iutdijon.u-bourgogne.fr/oge-esirem/"
-        self.URL_LOGIN = "https://casiut21.u-bourgogne.fr/cas-esirem/login?service=https%3A%2F%2Fiutdijon.u-bourgogne.fr%2Foge-esirem%2F"
-        self.URL_ABSENCES = "https://iutdijon.u-bourgogne.fr/oge-esirem/stylesheets/etu/absencesEtu.xhtml"
+        self.password = pwd
+        self.home_url = "https://iutdijon.u-bourgogne.fr/oge-esirem/"
+        self.login_url = "https://casiut21.u-bourgogne.fr/cas-esirem/login?service=https%3A%2F%2Fiutdijon.u-bourgogne.fr%2Foge-esirem%2F"
+        self.absences_url = "https://iutdijon.u-bourgogne.fr/oge-esirem/stylesheets/etu/absencesEtu.xhtml"
         self.session = requests.Session()
 
-        print("API created")
-        print("User: " + self.username)
-        print("Password: " + self.pwd)
-    
-    def get(self, url):
-        print("Get " + url)
-        return self.session.get(url)
-    
-    def login(self):
-        # GET request to fetch login form
-        response = self.session.get(self.URL_LOGIN)
-        execution = re.search(r'name="execution" value="([^"]+)"', response.text).group(1)
+        print(f"API created")
 
-        # POST request with login data
+    def get(self, url):
+        print(f"Get {url}")
+        return self.session.get(url)
+
+    def login(self):
+        response = self.session.get(self.login_url)
+        execution = re.search(r'name="execution" value="([^"]+)"', response.text).group(1)
         payload = {
             'username': self.username,
-            'password': self.pwd,
+            'password': self.password,
             'execution': execution,
             '_eventId': 'submit'
         }
-        login_response = self.session.post(self.URL_LOGIN, data=payload)
+        login_response = self.session.post(self.login_url, data=payload)
 
-        # Check if login was successful
         if "Connexion - CAS" not in login_response.text:
             print("Login successful")
-            print
             return True
         else:
             print("Login failed")
             return False
-    
+
     def getAbsencesPage(self):
         print("Get absences page...")
-        response = self.session.get(self.URL_ABSENCES)
+        response = self.session.get(self.absences_url)
         return response.text
-    
+
     def getAbsences(self):
         page = self.getAbsencesPage()
         soup = BeautifulSoup(page, 'html.parser')
-        
+
         absences_table = soup.find_all('tr', class_='ui-widget-content')
-        print("Found " + str(len(absences_table)) + " absences")
-        
+        print(f"Found {len(absences_table)} absences")
+
         absences = []
         for row in absences_table:
             columns = row.find_all('td', class_='ui-panelgrid-cell')
             absence_data = [column.get_text(strip=True) for column in columns]
             absences.append(absence_data)
-            
+
         return absences
