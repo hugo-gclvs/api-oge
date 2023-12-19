1ec4648572ec48acbf753b20f51fabf2b5e62834
Hugoreoo <hugo.gclvs@icloud.com>
2023-12-19 18:43:29 +0100

    Add templates: get_all_absences

--- OGEStatLab/absences/urls.py
+++ OGEStatLab/absences/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.welcome_page, name='welcome_page'),	
+	path('', views.welcome_page, name='welcome_page'),
+	path('get_all_absences/', views.get_all_absences, name='get_all_absences'),
 ]
--- OGEStatLab/absences/views.py
+++ OGEStatLab/absences/views.py
@@ -1,5 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from dotenv import load_dotenv
+from services.session_manager import SessionManager
+from services.oge_scraper import OgeScraper
+from services.absence_service import AbsenceService
+import os
+
+load_dotenv()
 
 def welcome_page(request):
     return HttpResponse("Bienvenue sur la page d'accueil de l'application web")
+
+def get_all_absences(request):
+    session_manager = SessionManager(user=os.getenv("API_USERNAME"), pwd=os.getenv("API_PASSWORD"))
+    
+    # Connexion
+    if session_manager.login():
+        oge_scraper = OgeScraper(session_manager)
+
+        absence_service = AbsenceService(oge_scraper)
+
+        try:
+            absences = absence_service.getAllAbsences()
+        except Exception as e:
+            absences = []
+
+    return render(request, 'absences/get_all_absences.html', {'absences': absences})
