4554b8daf25aecb8be877c575049f1c523ca82a7
Hugoreoo <hugo.gclvs@icloud.com>
2023-12-19 12:15:17 +0100

    Refactor main.py: Removed unnecessary comments and unused code

--- main.py
+++ main.py
@@ -8,25 +8,17 @@ from services.absence_service import AbsenceService
 load_dotenv()
 
 def main():
-    # Création de l'instance de SessionManager avec les identifiants
     session_manager = SessionManager(user=os.getenv("API_USERNAME"), pwd=os.getenv("API_PASSWORD"))
     
     # Connexion
     if session_manager.login():
-        # Initialisation de OgeScraper avec SessionManager
         oge_scraper = OgeScraper(session_manager)
 
-        # Initialisation des services avec OgeScraper
         absence_service = AbsenceService(oge_scraper)
-        # grade_service = GradeService(oge_scraper)
 
-        # Récupération des absences
         absences = absence_service.getAllAbsences()
         for absence in absences:
-            print(absence)  # Assurez-vous que la méthode __str__ est bien définie dans le modèle d'absence
-
-        # Récupération des notes (exemple, implémentez selon vos besoins)
-        # grades = grade_service.getGrades(semester)
+            print(absence)
 
     else:
         print("Échec de la connexion.")
